4200c1832f9f517d5db7d6bd054911b9e7b25f80
leeyaowen <jcjh30101@gmail.com>
2020-09-02 15:23:35 +0800

    check is int

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("a=?");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("b=?");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("a+b=" + (a+b));
+
+            //for(int i=1; i<=10; i=i+1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int k;
+            Console.WriteLine("Is Int?");
+            string s = Console.ReadLine();
+            if(int.TryParse(s,out k))
+            {
+                Console.WriteLine("is Int");
+            }
+            else
+            {
+                Console.WriteLine("not Int");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
